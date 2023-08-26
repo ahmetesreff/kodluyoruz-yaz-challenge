845621f3027966f91b4e6e2974892e12a679c51a
ahmetesreff <ahmetesreff@gmail.com>
2023-08-26 10:37:04 +0300

    challenge-3 added

---
+++ challenge-3.js
@@ -0,0 +1,9 @@
+const isabetliUcPuanAtis = 5;
+const isabetliIkiPuanAtis = 10;
+
+const ucPuan = 3;
+const ikiPuan = 2;
+
+const toplamPuan = (ucPuan * isabetliUcPuanAtis) + (ikiPuan * isabetliIkiPuanAtis);
+
+console.log("Toplam puan:", toplamPuan);
